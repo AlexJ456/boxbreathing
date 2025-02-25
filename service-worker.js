43e377b91aa2e1db66750dc9a4404eb02aef3a83
AlexJ456 <alexjohanson22@gmail.com>
2025-02-25 03:34:34 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,7 @@
-const CACHE_NAME = 'box-breathing-app-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/main.py',
-  '/manifest.json',
-  'https://pyscript.net/latest/pyscript.css',
-  'https://pyscript.net/latest/pyscript.js'
+const cacheName = 'box-breathing-app-v1';
+const assets = [
+  'index.html',
+  'main.py',
+  'manifest.json',
+  // Include PyScript assets
 ];
-
-// Install service worker and cache all assets
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-// Activate and clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.filter(name => name !== CACHE_NAME)
-            .map(name => caches.delete(name))
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
-
-// Serve from cache, fall back to network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then(response => {
-            // Don't cache responses that aren't successful
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            // Clone the response
-            const responseToCache = response.clone();
-            
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-              
-            return response;
-          });
-      })
-  );
-});
